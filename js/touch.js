62e820603e3528775bd12a071061bafd85eb5f2b
cristeaioan <c.ioan12@yahoo.com>
2018-12-14 09:35:17 -0800

    Add files via upload

--- js/touch.js
+++ js/touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version = 2018.12.14.0";
+//document.getElementById("id_logic_version").innerHTML = "Logic version = 2018.12.14.0";
 document.getElementById("id_canvas").addEventListener("touchstart", on_touch_start);
 
 function on_touch_start(e) {
